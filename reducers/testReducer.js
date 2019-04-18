b0d120d988bccbdbc4d99a2a1d1505a6c3da43f8
Royce Birnbaum <royce.birnbaum@gmail.com>
2019-04-18 16:08:40 -0700

    alerts

--- reducers/testReducer.js
+++ reducers/testReducer.js
@@ -4,6 +4,7 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case "updateTitle":
+      alert(action.payload)
       return {
         ...state,
         title: action.payload
